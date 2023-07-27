2dc28aa5b4a1f3d30cdfa92e69143e2531cadf0d
Ольга Карпова <tordvist@yandex.ru>
2023-07-27 21:27:54 +0300

    Fix flake8 errors

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,8 +5,8 @@ class IsAuthorOrReadOnlyPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
@@ -14,4 +14,3 @@ class IsAuthorOrReadOnlyPermission(permissions.BasePermission):
             request.method in permissions.SAFE_METHODS
             or obj.author == request.user
         )
-
